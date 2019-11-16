7494b20478a4a174bcec11812bf36f2f732e1a2f
Rosemary <rosey.ly@hotmail.com>
2019-11-16 15:29:05 -0500

    get cat

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 'use strict';
 
-let double = require('./double'); 
-console.log(double(4));
-console.log(double);
+let express = require('express');
+let getCat = require('./get-cat');
 
-let getCat = require ('/get-cat.js');
-console.log(getCat);
-getCat().then(afterGetCat);
+let app = express();
+let port = 3000;
+
+/**
+ * Three "app.get`s are functionally the same
+ */
+// app.get('/', function (req, res) {
+  // Code goes here
+// });
+
+// app.get('/', (req, res) => {
+  // Code goes here
+// });
+
+function handleGetHome(request, response) {
+  response.send('Hello, world!');
+}
+app.get('/', handleGetHome);
+
+/**
+ * GET /cat
+ */
+function handleGetCat(request, response) {
+  // This runs when the route (HTTP method + URL path) is requested
+  function handleCatResponse(catResponse) {
+    response.send(catResponse.data);
+  }
+  getCat().then(handleCatResponse);
+}
+app.get('/cat', handleGetCat); // We define the route here
+
+function handleServerListen() {
+  console.log(`Server is listening on port ${port}`);
+}
+app.listen(port, handleServerListen);
